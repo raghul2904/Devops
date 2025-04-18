88be203f80b4646bfadd7d82e218a2de8d6cf3fa
Suresh V <vjsuru94@gmail.com>
2025-04-18 23:48:19 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+ 
+ echo “Current Branch: $BRANCH_NAME”
+  
+  if [ “$BRANCH_NAME” == “dev” ]; then
+     docker build -t sureshv1994/dev-react-image .
+     echo “Suresh@1994” | docker login -u “sureshv1994” --password-stdin
+     docker push sureshv1994/dev-react-image:latest
+
+  elif [“$BRANCH_NAME” == “main” ]; then
+     docker build -t sureshv1994/dev-react-image .
+     echo “Suresh@1994” | docker login -u “sureshv1994” --password-stdin
+     docker push sureshv1994/dev-react-image:latest
+fi
+chmod +x build.sh
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Deployment Branch: $BRANCH_NAME"
+
+docker stop my-reactapp-container || true
+docker rm my-reactapp-container || true
+
+if [ "$BRANCH_NAME" == "dev" ]; then
+  docker pull sureshv1994/dev-react-image:latest
+  docker run -d -p 80:80 --name my-reactapp-container sureshv1994/dev-react-image:latest
+
+elif [ "$BRANCH_NAME" == "main" ]; then
+  docker pull sureshv1994/prod-react-image:latest
+  docker run -d -p 80:80 --name my-reactapp-container sureshv1994/prod-react-image:latest
+fi
+
+chmod +x deploy.sh
+

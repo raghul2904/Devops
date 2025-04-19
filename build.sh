e7f61174ba47eb996155ec1221ed4d738846a541
Suresh V <vjsuru94@gmail.com>
2025-04-20 01:55:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@
      docker push sureshv1994/dev-react-image:latest
 
   elif [“$BRANCH_NAME” == “main” ]; then
-     docker build -t sureshv1994/dev-react-image .
+     docker build -t sureshv1994/prod-react-image .
      echo “Suresh@1994” | docker login -u “sureshv1994” --password-stdin
-     docker push sureshv1994/dev-react-image:latest
+     docker push sureshv1994/prod-react-image:latest
 fi
 
